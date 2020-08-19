fc88f8ed764818cb9cb090030fc06a01e4c75dff
unknown <celsomutti@gmail.com>
2020-08-19 17:35:29 -0300

    implementação e testes com o novo provedor

--- dc_config.php
+++ dc_config.php
@@ -1,14 +1,18 @@
 <?php
 
-///$servername = "177.71.233.182";
-//$username = "root";
-//$password = "Al0c@rioca18";
-//$dbname = "bderpsisgef";
+header("Cache-Control: no-cache, no-store, must-revalidate"); // limpa o cache
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=utf-8"); 
 
-$servername = "201.38.172.137";
+$servername = "177.71.233.182";
 $username = "root";
 $password = "Al0c@rioca18";
 $dbname = "bderpsisgef";
+
+//$servername = "localhost";
+//$username = "root";
+//$password = "al0c@rioca";
+//$dbname = "bderpsisgef";
 try {
     	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- dc_login.php
+++ dc_login.php
@@ -2,10 +2,6 @@
 
    include 'dc_config.php';
    
-   header("Cache-Control: no-cache, no-store, must-revalidate"); // limpa o cache
-	 header("Access-Control-Allow-Origin: *");
-	 header("Content-Type: application/json; charset=utf-8"); 
-	 
 	 // Check whether username or password is set from android	
      if(isset($_POST['username']) && isset($_POST['password']))
      {
