21c26ebd902425bad059bbbb06b7d12baec297c6
unknown <celsomutti@gmail.com>
2021-01-19 10:30:15 -0300

    implementação do tipo de entrega no extrato

--- dc_entregas_consolidado.php
+++ dc_entregas_consolidado.php
@@ -15,7 +15,7 @@
           count(tbentregas.num_nossonumero) as qtd_entregas, tbentregas.num_extrato, tbentregas.des_tipo_peso as des_tipo  
           from tbentregas
           where tbentregas.cod_entregador in (' . $entregador . ') and tbentregas.dat_baixa between "' . $dataini . '" and 
-          "' . $datafim . '" group by tbentregas.cod_cliente_empresa, tbentregas.val_verba_entregador, tbentregas.des_tipo_peso;';
+          "' . $datafim . '" and dom_fechado <> "S" group by tbentregas.cod_cliente_empresa, tbentregas.val_verba_entregador;';
           $stmt = $conn->prepare($sql);
           $stmt->execute();
           if($stmt->rowCount())
