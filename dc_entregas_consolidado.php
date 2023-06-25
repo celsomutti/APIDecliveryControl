556236c2fa86cf10efd2da7b0f16cebc59eb1ac7
Celso Mutti <celsomutti@gmail.com>
2023-06-25 09:45:18 -0300

    ajuste nome do cliente no extrato prévio

--- dc_entregas_consolidado.php
+++ dc_entregas_consolidado.php
@@ -12,8 +12,11 @@
          		  
 		  // Query database for row exist or not
           $sql = 'select tbentregas.cod_cliente_empresa as cod_cliente, tbentregas.cod_entregador as cod_entregador, tbentregas.val_verba_entregador as val_verba, 
-          count(tbentregas.num_nossonumero) as qtd_entregas, tbentregas.num_extrato, tbentregas.des_tipo_peso as des_tipo  
-          from tbentregas
+          count(tbentregas.num_nossonumero) as qtd_entregas, tbentregas.num_extrato, tbentregas.des_tipo_peso as des_tipo,
+          crm_clientes.nom_fantasia as nom_cliente  
+          FROM tbentregas
+          INNER JOIN crm_clientes
+          ON crm_clientes.cod_cliente = tbentregas.cod_cliente_empresa
           where tbentregas.cod_entregador in (' . $entregador . ') and tbentregas.dat_baixa between "' . $dataini . '" and 
           "' . $datafim . '" and dom_fechado <> "S" group by tbentregas.cod_cliente_empresa, tbentregas.val_verba_entregador;';
           $stmt = $conn->prepare($sql);
